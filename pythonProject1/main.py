c78845126120d7a87a9f1d8984a749e7a0fd6588
kfest <yunusova.k.b@gmail.com>
2024-09-12 14:49:51 +0500

    tst

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -10,10 +10,3 @@ def count_elements(*lists):
         common_elements.intersection_update(i)
 
     return len(common_elements)
-
-list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 1]
-list3 = [4, 1, 6, 7]
-
-result = count_elements(list1, list2, list3)
-print(f"Количество одинаковых элементов: {result}")
---
+++ pythonProject1/test.py
@@ -0,0 +1,7 @@
+from main import count_elements
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 1]
+list3 = [4, 1, 6, 7]
+
+result = count_elements(list1, list2, list3)
+print(f"Количество одинаковых элементов: {result}")
